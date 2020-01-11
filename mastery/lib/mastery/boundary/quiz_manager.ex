aca5b40dfc5bd6575a3ceecc5274293d48e59b44
Brian Lubeski <bclube@gmail.com>
2020-01-11 15:28:28 -0500

    Reimplement quiz manager using Agent

--- mastery/lib/mastery/boundary/quiz_manager.ex
+++ mastery/lib/mastery/boundary/quiz_manager.ex
@@ -1,48 +1,30 @@
 defmodule Mastery.Boundary.QuizManager do
   alias Mastery.Core.Quiz
-  use GenServer
+  use Agent
 
   ### API
 
   def start_link(name \\ __MODULE__) do
-    GenServer.start_link(__MODULE__, %{}, name: name)
+    Agent.start_link(fn -> %{} end, name: name)
   end
 
   def build_quiz(name \\ __MODULE__, quiz_fields) do
     quiz = Quiz.new(quiz_fields)
 
-    GenServer.call(name, {:add_quiz, quiz})
+    Agent.update(name, &Map.put(&1, quiz.title, quiz))
   end
 
   def add_template(name \\ __MODULE__, quiz_title, template_fields) do
-    GenServer.call(name, {:add_template, quiz_title, template_fields})
+    Agent.get_and_update(
+      name,
+      &Map.get_and_update(&1, quiz_title, fn
+        nil -> :pop
+        quiz -> {:ok, Quiz.add_template(quiz, template_fields)}
+      end)
+    ) || :error
   end
 
   def lookup_quiz_by_title(name \\ __MODULE__, quiz_title) do
-    GenServer.call(name, {:lookup_quiz_by_title, quiz_title})
-  end
-
-  ### Callbacks
-
-  @impl true
-  def init(quizzes) when is_map(quizzes) do
-    {:ok, quizzes}
-  end
-
-  @impl true
-  def handle_call({:add_quiz, quiz}, _from, quizzes) do
-    new_quizzes = Map.put(quizzes, quiz.title, quiz)
-
-    {:reply, :ok, new_quizzes}
-  end
-
-  def handle_call({:add_template, quiz_title, template_fields}, _from, quizzes) do
-    new_quizzes = Map.update!(quizzes, quiz_title, &Quiz.add_template(&1, template_fields))
-
-    {:reply, :ok, new_quizzes}
-  end
-
-  def handle_call({:lookup_quiz_by_title, quiz_title}, _from, quizzes) do
-    {:reply, quizzes[quiz_title], quizzes}
+    Agent.get(name, &Map.get(&1, quiz_title))
   end
 end
