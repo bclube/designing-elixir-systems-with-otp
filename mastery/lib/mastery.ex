5f5dade5f1e923a9a5a2d722dc3d30a7b278fac6
Brian Lubeski <bclube@gmail.com>
2020-01-17 23:42:17 -0500

    Create API layer

--- mastery/lib/mastery.ex
+++ mastery/lib/mastery.ex
@@ -1,18 +1,41 @@
 defmodule Mastery do
-  @moduledoc """
-  Documentation for Mastery.
-  """
+  alias Mastery.Boundary.{
+    QuizManager,
+    QuizSession,
+    QuizValidator,
+    TemplateValidator
+  }
 
-  @doc """
-  Hello world.
+  alias Mastery.Core.Quiz
 
-  ## Examples
+  def start_quiz_manager() do
+    QuizManager.start_link()
+  end
 
-      iex> Mastery.hello()
-      :world
+  def build_quiz(fields) do
+    with :ok <- QuizValidator.errors(fields),
+         do: QuizManager.build_quiz(fields),
+         else: (error -> error)
+  end
 
-  """
-  def hello do
-    :world
+  def build_template(title, fields) do
+    with :ok <- TemplateValidator.errors(fields),
+         do: QuizManager.add_template(title, fields),
+         else: (error -> error)
+  end
+
+  def take_quiz(title, email) do
+    with %Quiz{} = quiz <- QuizManager.lookup_quiz_by_title(title),
+         {:ok, session} <- QuizSession.start_link(quiz, email),
+         do: session,
+         else: (error -> error)
+  end
+
+  def select_question(session) do
+    QuizSession.select_question(session)
+  end
+
+  def answer_question(session, answer) do
+    QuizSession.answer_question(session, answer)
   end
 end
